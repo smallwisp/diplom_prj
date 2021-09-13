5efd7dc8d5de2d98e81cd1475f4a3228cd31bb94
smallwisp <4ubarov96@mail.ru>
2021-09-13 20:17:20 +0300

    created a modular project structure using webpack and added toggleModal.js

---
+++ dist/main.bc95d55ec0794e472607.hot-update.js
@@ -0,0 +1,29 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+self["webpackHotUpdatediplom"]("main",{
+
+/***/ "./src/modules/toggleModal.js":
+/*!************************************!*\
+  !*** ./src/modules/toggleModal.js ***!
+  \************************************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nSyntaxError: D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\src\\\\modules\\\\toggleModal.js: 'Const declarations' require an initialization value. (6:12)\\n\\n\\u001b[0m \\u001b[90m 4 |\\u001b[39m           modalOverlay \\u001b[33m=\\u001b[39m document\\u001b[33m.\\u001b[39mquerySelector(\\u001b[32m'.modal-overlay'\\u001b[39m)\\u001b[33m,\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 5 |\\u001b[39m\\u001b[0m\\n\\u001b[0m\\u001b[31m\\u001b[1m>\\u001b[22m\\u001b[39m\\u001b[90m 6 |\\u001b[39m     document\\u001b[33m.\\u001b[39maddEventListener(\\u001b[32m'click'\\u001b[39m\\u001b[33m,\\u001b[39m (event) \\u001b[33m=>\\u001b[39m {\\u001b[0m\\n\\u001b[0m \\u001b[90m   |\\u001b[39m             \\u001b[31m\\u001b[1m^\\u001b[22m\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 7 |\\u001b[39m         console\\u001b[33m.\\u001b[39mlog(event\\u001b[33m.\\u001b[39mtarget)\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 8 |\\u001b[39m         \\u001b[36mlet\\u001b[39m target \\u001b[33m=\\u001b[39m event\\u001b[33m.\\u001b[39mtarget\\u001b[33m;\\u001b[39m\\u001b[0m\\n\\u001b[0m \\u001b[90m 9 |\\u001b[39m         \\u001b[36mif\\u001b[39m (target\\u001b[33m.\\u001b[39mclassList\\u001b[33m.\\u001b[39mcontains(\\u001b[32m'callback-btn'\\u001b[39m)) {\\u001b[0m\\n    at Parser._raise (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:808:17)\\n    at Parser.raiseWithData (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:801:17)\\n    at Parser.raise (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:762:17)\\n    at Parser.parseVar (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13503:18)\\n    at Parser.parseVarStatement (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13313:10)\\n    at Parser.parseStatementContent (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12896:21)\\n    at Parser.parseStatement (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:12829:17)\\n    at Parser.parseBlockOrModuleBlockBody (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13418:25)\\n    at Parser.parseBlockBody (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13409:10)\\n    at Parser.parseBlock (D:\\\\программирование,верстка\\\\Курс JS 20.0\\\\diplom\\\\node_modules\\\\@babel\\\\parser\\\\lib\\\\index.js:13393:10)\");\n\n//# sourceURL=webpack://diplom/./src/modules/toggleModal.js?");
+
+/***/ })
+
+},
+/******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/getFullHash */
+/******/ (() => {
+/******/ 	__webpack_require__.h = () => ("b951026d6ca3dc1c039b")
+/******/ })();
+/******/ 
+/******/ }
+);
--- src/modules/toggleModal.js
+++ src/modules/toggleModal.js
@@ -1,7 +1,47 @@
 const toggleModal = () => {
+    const modalCallback = document.querySelector('.modal-callback'),
+          modalOverlay = document.querySelector('.modal-overlay');
 
-    const callbackBtn = document.querySelectorAll('.callback-btn');
-    console.log(callbackBtn);
+    document.addEventListener('click', (event) => {
+        console.log(event.target);
+        let target = event.target;
+        if (target.classList.contains('callback-btn')) {
+            modalCallback.style.display = 'block';
+            modalOverlay.style.display = 'block';
+        } else if (target.closest('.modal-close') || target.classList.contains('modal-overlay')) {
+            modalCallback.style.display = 'none';
+            modalOverlay.style.display = 'none';
+        };
+
+
+    });
+    /* callbackBtn.forEach((item) => {
+        item.addEventListener('click', () => {
+            modalCallback.style.display = 'block';
+            modalOverlay.style.display = 'block';
+        })
+    });
+
+    modalCallback.addEventListener('click', event => {
+            let target = event.target;
+
+            if (target.classList.contains('modal-close')) {
+                modalCallback.style.display = 'none';
+                modalOverlay.style.display = 'none';
+            } else {
+
+            }
+
+    });
+
+    modalOverlay.addEventListener('click', (event) => {
+        
+            modalCallback.style.display = 'none';
+            modalOverlay.style.display = 'none';
+        // }
+            
+    }) */
+
 };
 
 export default toggleModal;
